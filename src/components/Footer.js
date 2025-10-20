5d2451a5ab8c75af7fb959e11f95dae79e06d787
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-10-20 23:20:41 +0530

    Added MoviesGrid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ import "../styles.css";
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <>
-      <footer className="footer">
-        <p className="footer-text">© {year} Moviedux, All Rights Reserved.</p>
-      </footer>
-    </>
+    <footer className="footer">
+      <p className="footer-text">© {year} Moviedux, All Rights Reserved.</p>
+    </footer>
   );
 }
